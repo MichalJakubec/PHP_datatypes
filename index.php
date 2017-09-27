7f3d67f0822f236c966d3f7bbec6e189ab94fabb
Michal Jakubec <mr.jakubec@gmail.com>
2017-09-27 16:48:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $var = 123;
+    $datatype = var_dump($var);
+    echo $datatype . "<br />";
+    $var2 = 'halo';
+    $datatype2 = var_dump($var2);
+    echo $datatype2 . "<br />";
+    $var3 = true;
+    $datatype3 = var_dump($var3);
+    echo $datatype3 . "<br />";
+    $var4 = 14.5;
+    $datatype4 = var_dump($var4);
+    echo $datatype4 . "<br />";
+    $var5 = ['abc', 'def', 'ghi'];
+    $datatype5 = var_dump($var5);
+    echo $datatype5 . "<br />";
+    $var6 = Null;
+    $datatype6 = var_dump($var6);
+    echo $datatype6;
+?>
